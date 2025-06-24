bdd67197ce9f05f4c2fb910487231e924da85005
skysunm <ivadream100@naver.com>
2025-06-24 10:26:14 +0900

    feat: Home_Hero 섹션 구현

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/Hero.css';
 import heroImage from '../assets/홈_상단사진.png';
 
 const Hero = () => {
   return (
-    <section>
-      <img src={heroImage} alt="숙소 전경" style={{ width: '100%' }} />
+    <section className="hero-section">
+      <img src={heroImage} alt="숙소 전경" className="hero-image" />
     </section>
   );
 };
