63fa6520068983d6397ed059d9da825725ed5230
joyfulond <kimhy030927@gmail.com>
2025-06-25 18:38:38 +0900

    마이페이지 구현 및 링크 연결

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,13 +6,13 @@ import logo from '../assets/images/할머니로고.png';
 const Header = () => {
   return (
     <header>
-      <div className='logo-container'>
+      <Link to='/' className='logo-container'>
         <img src={logo} alt='할매의 손맛 로고' className='logo-image' />
         <div className='logo-text-section'>
           <span className='logo-text'>할매의 손맛</span>
           <div className='logo-subtitle'>홈스테이 · 따뜻한밥상 · 농촌체험</div>
         </div>
-      </div>
+      </Link>
       <nav>
         <ul>
           <li>
