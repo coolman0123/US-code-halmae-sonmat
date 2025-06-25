b63ea9178db873ec05ee4b89a1033631039ccec3
joyfulond <kimhy030927@gmail.com>
2025-06-25 16:51:35 +0900

    마이페이지 넷바 연결 및 넷바 링크 수정

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,10 +22,13 @@ const Header = () => {
             <Link to='/experiences'>특별한 체험</Link>
           </li>
           <li>
-            <Link to='/reservation'>예약안내</Link>
+            <Link to='/Booking'>예약안내</Link>
           </li>
           <li>
             <Link to='/live-reservation'>실시간예약</Link>
+          </li>
+          <li>
+            <Link to='/mypage'>마이페이지</Link>
           </li>
           <li>
             <Link to='/login'>로그인</Link>
--- frontend/src/pages/MyPage/MyPage/MyPage.jsx
+++ frontend/src/pages/MyPage/MyPage/MyPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../../../components/Header';
+import Footer from '../../../components/Footer';
+import './MyPage.css';
 
 const MyPage = () => {
   return (
     <div>
+      <Header />
       <h1>마이페이지</h1>
     </div>
   );
