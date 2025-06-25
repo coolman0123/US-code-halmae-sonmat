90816a980537d56ac93b9d5744529481fd79386d
joyfulond <kimhy030927@gmail.com>
2025-06-26 01:35:52 +0900

    체크버튼 반응형 구현 및 리뷰 작성 페이지 구현

--- frontend/src/pages/MyPage/MyReview/WriteReview/WriteReview.jsx
+++ frontend/src/pages/MyPage/MyReview/WriteReview/WriteReview.jsx
@@ -9,25 +9,36 @@ const mockReservations = [
     id: 1,
     location: '여여',
     date: '2025.06.24',
-    desc: '"말보단 손이 빠른" 박봉순 할머니',
+    description: '"말보단 손이 빠른" 박봉순 할머니',
+    price: '340,000원',
   },
   {
     id: 2,
-    location: '여여',
-    date: '2025.06.24',
-    desc: '"말보단 손이 빠른" 박봉순 할머니',
+    location: '모모',
+    date: '2025.05.24',
+    description: '"입은 좀 험하지만 속은 꿀" 김옥순 할머니',
+    price: '150,000원',
   },
   {
     id: 3,
-    location: '여여',
-    date: '2025.06.24',
-    desc: '"말보단 손이 빠른" 박봉순 할머니',
+    location: '소소',
+    date: '2025.04.14',
+    description: '"전쟁통에도 솥은 놓지 않았다" 이금자 할머니',
+    price: '150,000원',
   },
   {
     id: 4,
-    location: '여여',
-    date: '2025.06.24',
-    desc: '"말보단 손이 빠른" 박봉순 할머니',
+    location: '호호',
+    date: '2025.03.14',
+    description: '"한 마디면 눈물 터지는" 정다감 할머니',
+    price: '170,000원',
+  },
+  {
+    id: 5,
+    location: '패밀리',
+    date: '2025.03.02',
+    description: '"메뉴는 고정, 맛은 고정불변" 조말순 할머니',
+    price: '150,000원',
   },
 ];
 
@@ -61,7 +72,6 @@ const WriteReview = () => {
           onClick={() => {
             if (selectedId) {
               console.log('리뷰 작성하러 가기!');
-              // TODO: 이동 로직 연결
             }
           }}
           text='리뷰 작성하러 가기'
