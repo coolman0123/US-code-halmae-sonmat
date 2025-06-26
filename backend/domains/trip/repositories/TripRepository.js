3d50fdc8c2dccdb14ca427da8a6d8fbc9f0b325a
coolman0123 <socool4m@naver.com>
2025-06-26 12:50:05 +0900

    feat: triprepo 여행 생성

---
+++ backend/domains/trip/repositories/TripRepository.js
@@ -0,0 +1,20 @@
+const FirebaseRepository = require('../../../firebase/FirebaseRepository.js');
+const admin = require('../../../firebase/admin.js');
+const Trip = require('../entities/Trip.js');
+
+class TripRepository extends FirebaseRepository {
+  constructor() {
+    super('trips');
+  }
+
+  async create(tripData) {
+    const trip = new Trip(tripData);
+    const obj = trip.toJSON();
+    delete obj.id; 
+    const docRef = await this.collection.add(obj);
+    return { id: docRef.id, ...obj };
+  }
+
+}
+
+module.exports = TripRepository;
