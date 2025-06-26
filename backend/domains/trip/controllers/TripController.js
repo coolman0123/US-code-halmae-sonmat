6fd5840c01342ec6135b15449bb5fb03190716ff
coolman0123 <socool4m@naver.com>
2025-06-26 13:36:14 +0900

    feat:  trip update controller

--- backend/domains/trip/controllers/TripController.js
+++ backend/domains/trip/controllers/TripController.js
@@ -55,6 +55,21 @@ class TripController {
       next(error);
     }
   }
+  async updateTrip(req, res, next) {
+    try {
+      const { id } = req.params;
+      const trip = await this.tripService.updateTrip(id, req.body);
+      
+      res.status(200).json({
+        success: true,
+        message: '여행 정보가 수정되었습니다.',
+        data: trip
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
 
 }
 
