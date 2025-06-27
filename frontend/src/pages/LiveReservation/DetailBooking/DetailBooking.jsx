4cd370575c25310dbb19d1a327cf90ff53849b9c
joyfulond <kimhy030927@gmail.com>
2025-06-27 23:44:47 +0900

    마이페이지 수정 및 실시간예약 폴더 생성

---
+++ frontend/src/pages/LiveReservation/DetailBooking/DetailBooking.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailBooking = () => {
+  return (
+    <div>
+      <h1>예약 상세 페이지</h1>
+    </div>
+  );
+};
+
+export default DetailBooking;
---
+++ frontend/src/pages/LiveReservation/MyReservation/MyReservation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyReservation = () => {
+  return (
+    <div>
+      <h1>예약 현황 페이지</h1>
+    </div>
+  );
+};
+
+export default MyReservation;
