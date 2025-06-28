12af77b20dee0f3863e7d8d0f0e7dc4c90c9a727
joyfulond <kimhy030927@gmail.com>
2025-06-28 15:46:19 +0900

    디자인 수정

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Modal.css';
+import React from "react";
+import "./Modal.css";
 
 const Modal = ({ children, onClose }) => {
   return (
@@ -14,4 +14,4 @@ const Modal = ({ children, onClose }) => {
   );
 };
 
-export default Modal; 
+export default Modal;
--- frontend/src/pages/MyPage/MyPage/MyPage.jsx
+++ frontend/src/pages/MyPage/MyPage/MyPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './MyPage.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./MyPage.css";
 
-import ProfileIcon from '../../../assets/icons/MyPage_프로필.png';
-import PaymentIcon from '../../../assets/icons/MyPage_내결제.png';
-import ReviewIcon from '../../../assets/icons/MyPage_내리뷰.png';
-import NotificationIcon from '../../../assets/icons/MyPage_알림.png';
+import ProfileIcon from "../../../assets/icons/MyPage_프로필.png";
+import PaymentIcon from "../../../assets/icons/MyPage_내결제.png";
+import ReviewIcon from "../../../assets/icons/MyPage_내리뷰.png";
+import NotificationIcon from "../../../assets/icons/MyPage_알림.png";
 
 const MyPage = () => {
   const navigate = useNavigate();
@@ -15,50 +15,47 @@ const MyPage = () => {
   };
 
   const handleInquiry = () => {
-    alert('관리자 전화번호: 010-1234-5678');
+    alert("관리자 전화번호: 010-1234-5678");
   };
 
   return (
-    <div className='my-page'>
-      <div className='my-page-content'>
-        <div className='profile-section'>
-          <img src={ProfileIcon} alt='프로필' className='profile-icon' />
-          <span className='welcome-message'>
+    <div className="my-page-bg">
+      <div className="my-page-card">
+        <div className="profile-section centered">
+          <img src={ProfileIcon} alt="프로필" className="profile-icon" />
+          <span className="welcome-message">
             <strong>김희연</strong> 님 환영합니다 :)
           </span>
         </div>
 
-        <div className='icon-buttons'>
+        <div className="icon-buttons centered">
           <div
-            className='icon-button'
-            onClick={() => handleNavigation('/mypage/payment')}
+            className="icon-button"
+            onClick={() => handleNavigation("/mypage/payment")}
           >
-            <img src={PaymentIcon} alt='내 결제' />
+            <img src={PaymentIcon} alt="내 결제" />
             <p>내 결제</p>
           </div>
           <div
-            className='icon-button'
-            onClick={() => handleNavigation('/mypage/review')}
+            className="icon-button"
+            onClick={() => handleNavigation("/mypage/review")}
           >
-            <img src={ReviewIcon} alt='내 리뷰' />
+            <img src={ReviewIcon} alt="내 리뷰" />
             <p>내 리뷰</p>
           </div>
           <div
-            className='icon-button'
-            onClick={() => handleNavigation('/mypage/notification')}
+            className="icon-button"
+            onClick={() => handleNavigation("/mypage/notification")}
           >
-            <img src={NotificationIcon} alt='알림함' />
+            <img src={NotificationIcon} alt="알림함" />
             <p>알림함</p>
           </div>
         </div>
 
-        <div className='mypage-links'>
-          <p className='inquiry' onClick={handleInquiry}>
+        <div className="mypage-links centered">
+          <button className="inquiry-btn" onClick={handleInquiry}>
             1 : 1 문의
-          </p>
-          <p className='logout' onClick={() => handleNavigation('/auth/logout')}>
-            로그아웃
-          </p>
+          </button>
         </div>
       </div>
     </div>
